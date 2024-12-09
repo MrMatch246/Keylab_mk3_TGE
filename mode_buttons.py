b5b992cce46f25e948cc59510b0d83e4f79c693b
Daniel Erceg <s8daerce@stud.uni-saarland.de>
2024-12-09 21:46:56 +0100

    Initial Commit

---
+++ mode_buttons.py
@@ -0,0 +1,16 @@
+# Decompiled with PyLingual (https://pylingual.io)
+# Internal filename: ..\..\..\output\Live\win_64_static\Release\python-bundle\MIDI Remote Scripts\KeyLab_mk3\mode_buttons.py
+# Bytecode version: 3.11a7e (3495)
+# Source timestamp: 2024-09-26 15:27:47 UTC (1727364467)
+
+from ableton.v3.control_surface import Component
+from ableton.v3.control_surface.controls import ButtonControl
+from ableton.v3.control_surface.display import Renderable
+
+class ModeButtonsComponent(Component, Renderable):
+    pass
+    device_button = ButtonControl()
+    mixer_button = ButtonControl()
+
+    def __init__(self, *a, **k):
+        super().__init__(*a, name='Mode_Buttons', **k)
